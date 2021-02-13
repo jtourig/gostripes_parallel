c8ecb5fad14eea3fe5c3f0ab0fb99886fdf43c18
rpolicastro <rpolicas@indiana.edu>
2021-02-13 18:20:07 -0500

    switching to command line arguments

--- preprocessing.sh
+++ preprocessing.sh
@@ -12,27 +12,47 @@
 ## module load sra-toolkit
 ## fasterq-dump -S -O seqs SRR10759419
 
-## Variables.
+## Command line arguments.
 
-CORES=4
-
-WORKDIR='.'
-SAMPLES='sample_sheet.csv'
-
-GTF='ftp://ftp.ensembl.org/pub/release-102/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.102.gtf.gz'
-ASSEMBLY='ftp://ftp.ensembl.org/pub/release-102/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna_sm.toplevel.fa.gz'
+while [[ $# -gt 0 ]]; do
+  key=$1
+  case $key in
+    -s|--sample-sheet)
+      SAMPLES="$2"
+      shift 2
+      ;;
+    -as|--assembly)
+      ASSEMBLY="$2"
+      shift 2
+      ;;
+    -an|--annotation)
+      GTF="$2"
+      shift 2
+      ;;
+    -o|--out-dir)
+      WORKDIR="$2"
+      shift 2
+      ;;
+    -t|--threads)
+      CORES="$2"
+      shift 2
+      ;;
+  esac
+done
 
 ## Setup.
 
 cd $WORKDIR
 export CORES
-export SAMPLES
 
 ## Preliminary fastq quality control.
 
 if [ ! -d seqs_qc ]; then mkdir seq_qc; fi
 
-find seqs -name "*fastq" | xargs fastqc -o seqs_qc -t $CORES
+cat  \
+  <(csvtk cut -f fastq_1 $SAMPLES | csvtk del-header) \
+  <(csvtk cut -f fastq_2 $SAMPLES | csvtk del-header) | \
+  xargs fastqc -o seqs_qc -t $CORES
 
 ## Keep only R1 reeds with N8TATAG3, and remove UMI, spacer, and poly-G.
 ## Tolerate 1 mismatched base.
@@ -59,22 +79,16 @@ if [ ! -d processed_qc ]; then mkdir processed_qc; fi
 
 find processed -name "*fastq" | xargs fastqc -o processed_qc -t $CORES
 
-## Download genome.
-
-if [ ! -d genome ]; then mkdir genome; fi
-
-parallel -j$CORES 'curl {} | gunzip > genome/{/.}' ::: $GTF $ASSEMBLY
-
 ## Create STAR genome index.
 
-if [ ! -d genome/index ]; then mkdir genome/index; fi
+if [ ! -d index ]; then mkdir index; fi
 
 STAR \
   --runThreadN $CORES \
   --runMode genomeGenerate \
-  --genomeDir genome/index \
-  --genomeFastaFiles genome/$(basename $ASSEMBLY .gz) \
-  --sjdbGTFfile genome/$(basename $GTF .gz) \
+  --genomeDir index \
+  --genomeFastaFiles genome/$(basename $ASSEMBLY) \
+  --sjdbGTFfile genome/$(basename $GTF) \
   --genomeSAindexNbases 10
 
 ## Align with STAR.
@@ -87,7 +101,7 @@ parallel --link \
   -a <(csvtk cut -f name $SAMPLES | csvtk del-header) \
   'STAR \
     --runThreadN $CORES \
-    --genomeDir genome/index \
+    --genomeDir index \
     --readFilesIn {1} {2} \
     --outFileNamePrefix aligned/{3}_'
 
