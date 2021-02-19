8b2fa00cad4d3562951568a978e5c7dfbc70627a
rpolicastro <rpolicas@indiana.edu>
2021-02-19 18:55:17 -0500

    added umi_tools dedup to single-end data

--- bin/samtools.sh
+++ bin/samtools.sh
@@ -13,6 +13,9 @@ SAMTOOLS_PAIRED() {
 SAMTOOLS_SINGLE() {
   printf "%-5s%s" "..." "Processing ${1}"
   samtools sort -@ $CORES bams/aligned/${1}_Aligned.out.sam | \
-    samtools view -F 2820 -O BAM -@ $CORES \
-    -o bams/cleaned/${1}.bam
+    samtools view -F 2820 -O BAM -@ $CORES | \
+    umi_tools dedup \
+      --output-stats=bams/cleaned/${1}_dedup_stats.txt \
+      --log=bams/cleaned/${1}_dedup_log.txt \
+      -S bams/cleaned/${1}.bam
 }
